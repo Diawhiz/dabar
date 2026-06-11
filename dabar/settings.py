a9e6d8c5d01174cf00004e1fd1dd0a27850d652d
Precious Olonade <preshegzy@gmail.com>
2026-06-11 22:08:53 +0100

    chore: add celery and redis configuration

--- dabar/settings.py
+++ dabar/settings.py
@@ -101,3 +101,12 @@ REST_FRAMEWORK = {
         "rest_framework.parsers.MultiPartParser",
     ],
 }
+
+REDIS_URL = config("REDIS_URL", default="redis://localhost:6379/0")
+
+CELERY_BROKER_URL = REDIS_URL
+CELERY_RESULT_BACKEND = REDIS_URL
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
+CELERY_TIMEZONE = TIME_ZONE
